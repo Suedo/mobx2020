f3e9e06cf061b1e547e331d1ee66eff4aef9f7c0
snag <somjitnag192@gmail.com>
2020-06-09 01:25:57 +0530

    editor style changes

--- src/components/EditorComponent/StyleButton.tsx
+++ src/components/EditorComponent/StyleButton.tsx
@@ -1,5 +1,15 @@
 import React, { FunctionComponent } from 'react';
-import { FormatBold, FormatItalic, FormatUnderlined, CodeSharp, LinkSharp } from '@material-ui/icons';
+import {
+  FormatBold,
+  FormatItalic,
+  FormatUnderlined,
+  Code,
+  Link,
+  FormatQuote,
+  FormatListBulleted,
+  FormatListNumbered,
+  DeveloperMode,
+} from '@material-ui/icons';
 import IconButton from '@material-ui/core/IconButton';
 
 interface StyleButtonPropsI {
@@ -31,9 +41,17 @@ const getIconIfValid = (label: any) => {
     case 'Underline':
       return <FormatUnderlined />;
     case 'Monospace':
-      return <CodeSharp />;
+      return <Code />;
     case 'Link':
-      return <LinkSharp />;
+      return <Link />;
+    case 'Blockquote':
+      return <FormatQuote />;
+    case 'UL':
+      return <FormatListBulleted />;
+    case 'OL':
+      return <FormatListNumbered />;
+    case 'Code Block':
+      return <DeveloperMode />;
     default:
       return label;
   }
